e2a21af7017beb9aa8579d9b564d2ae3805ee0d7
Higor Castilho <castilhohf@gmail.com>
2020-09-11 23:37:40 -0300

    fix: remove duplicated files

--- src/utils/convertHourToMinutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function convertHourToMinutes(time) {
-    const [hour, minutes] = time.split(':').map(Number); //destructing an array
-    const timeInMinutes = (hour * 60) + minutes;
-    return timeInMinutes;
-}
-exports.default = convertHourToMinutes;
